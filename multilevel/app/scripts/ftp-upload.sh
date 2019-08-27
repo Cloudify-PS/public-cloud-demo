a5554dfe92a5a20f7a7e53a285382707b6dafe12
Isaac Shabtay <isaac@shabtay.com>
2019-08-27 10:25:20 -0700

    Externalized multilevel blueprints to their own repositories

--- multilevel/app/scripts/ftp-upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-tempdir=$(mktemp -d)
-ctx download-resource-and-render ${resource_path} ${tempdir}/index.html
-lftp -e "put -a ${tempdir}/index.html" -u ${username},${password} ${endpoint}
-rm -rf ${tempdir}
